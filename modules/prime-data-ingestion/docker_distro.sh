2a2643434273eaca977e4b1d9ba7f86183478b83
Josiah Siegel <josiah.siegel@semanticbits.com>
2022-01-27 15:56:35 -0500

    include new open-source module

---
+++ modules/prime-data-ingestion/docker_distro.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+wsl --set-default $1
+
+echo "Restarting docker"
+powershell -c 'Stop-Process -Name "Docker Desktop"'
+sleep 1
+powershell -c 'Start-Process "C:\Program Files\Docker\Docker\Docker Desktop.exe"'
+sleep 1
+
+echo "Waiting for docker..."
+wsl -d $1 -e bash -c \
+' \
+until usermod -aG docker '"$3"'; do sleep 5; done \
+' &>/dev/null
+
+wsl -d $1 -u $3 -e bash -c \
+' \
+until docker info; do sleep 5; done \
+' &>/dev/null
+
+echo "Docker restart complete"
